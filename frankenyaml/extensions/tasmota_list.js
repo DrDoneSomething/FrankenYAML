852e3e79de260231eaf67c63c077112e8eefbee3
ManuelAntonioNoriegaMoreno <46264089+ManuelAntonioNoriegaMoreno@users.noreply.github.com>
2020-05-21 21:51:44 -0700

    Add files via upload

---
+++ frankenyaml/extensions/tasmota_list.js
@@ -0,0 +1,565 @@
+
+/** Tasmota list javascript
+ ** frankenyaml_javascript.js
+ ** by Dr DoneSomething
+ **/
+ 
+var tasmota_hostnames = {};
+var tasmota_all_click_buttons = [];
+var tasmota_exec_queue = [];
+var tasmota_queue_timer = false;
+var tasmota_queue_spacing = 100;
+function tasmota_manual_command(return_id,mode,cmnd)
+{
+    
+    if(!tasmota_exec_vars)
+    {
+        console.log("ERROR: tasmota_exec_vars not set");
+        return false;
+    }
+    var send_js =js_output=tooltip_output= false;
+    
+    var return_text = "";
+    var gets = Object.assign({},tasmota_exec_vars);
+    var exfpath = gets['exfpath'];
+    delete gets['exfpath'];
+    var id_prefix = return_id + "_tasmota_cmnd_";
+    
+    var textbox = document.getElementById(id_prefix+"text");
+    var checkbox = document.getElementById(id_prefix+"checkbox");
+    var button = document.getElementById(id_prefix+"button");
+    var tooltip = document.getElementById(id_prefix+"tooltip");
+    var checkbox_tooltip =document.getElementById(id_prefix+"checkbox_tooltip");
+    
+    var id_error = " id not found: "+id_prefix;
+    var error = false;
+    if(!textbox)
+        error = id_error+"text";
+    if(!checkbox)
+        error = id_error+"checkbox";
+    if(!button)
+        error = id_error+"button";
+    if(!tooltip)
+        error = id_error+"tooltip";
+    if(!checkbox_tooltip)
+        error = id_error+"checkbox_tooltip";
+    if(error)
+    {
+        console.log("FAIL: tasmota_manual_command  "+error);
+        return false;
+    }
+    
+    
+    switch(mode)
+    {
+        case 'get_reference':
+            if(!checkbox.checked)
+            {
+                set_element_disabled(button,true);
+                return;
+            }
+            console.log("get reference");
+            if(!textbox.value)
+            {
+                js_output = "[command blank]";
+                break;
+            }
+            send_js = true;
+            
+            gets['cmnd_reference'] = textbox.value;
+            set_element_disabled(button,false);
+            button.value = textbox.value;
+            checkbox_tooltip.style.display = "none";
+        break;
+        case 'insert':
+            textbox.value = cmnd;
+            checkbox.checked = false;
+            set_element_disabled(button,true);
+            checkbox_tooltip.style.display = "initial";
+        break;
+        case 'receive_reference':
+        
+            var check_keys = ['tooltip','parsed'];
+            for(var i in check_keys)
+            {
+                if(!(check_keys[i] in cmnd))
+                {
+                    console.log("FAIL: key "+check_keys[i]+" not found in cmnd ");
+                    console.log(cmnd);
+                    js_output = "[Error]";
+                    break;
+                }
+            }
+            if(!cmnd['parsed']['found'])
+            {
+                
+                return_text = "No reference found for "+textbox.value;
+                break;
+                
+            }
+            if(cmnd['parsed']['general_error'])
+            {
+                return_text = cmnd['parsed']['general_error'];
+                break;
+            }
+            return_text = cmnd['parsed']['text'];
+            if(!return_text)
+            {
+                return_text = "[Error: no parsed -> text]";
+                console.log(return_text);
+                console.log(cmnd)
+            }
+            
+        break;
+        case 'textbox_update':
+            checkbox.checked = false;
+            set_element_disabled(button,true);
+            checkbox_tooltip.style.display = "none";
+            button.value = cmnd;
+            checkbox.value = cmnd;
+        break;
+        default:
+        console.log("FAIL: tasmota_manual_command INVALID MODE: "+mode);
+        return false;
+    }
+    if(send_js)
+    {
+        gets['return_id'] = encodeURIComponent(return_id);
+    
+        var js_url = exfpath+"?"
+        
+        for(var i in gets)
+        {
+            if(!gets[i])
+                error += " no "+i+" found in gets";
+            js_url+=i+"="+gets[i]+"&";
+        }
+    
+        if(error)
+        {
+            console.log("FAIL: tasmota_manual_command  "+error);
+            return false;
+        }
+        loadScriptAndShowPending(return_id,js_url,"Reference "+cmnd);
+    }
+    if(return_text)
+    {
+        tooltip_output = js_output = return_text;
+    }
+    if(js_output)
+    {
+        var formatted = '<hr align="left" width="200"><span class="tasmota_reference_output">';
+        formatted += "Reference: "
+        formatted += js_output+'</span><hr align="left" width="200">';
+        
+        var tev = Object.assign({},tasmota_exec_vars);
+        if(tev['display_mode']=="short")
+            master_dump_append(formatted);
+        else
+            append_to_inner(return_id+"_td_js_output",formatted);
+    }
+    if(tooltip_output)
+    {
+        tooltip.innerHTML = tooltip_output;
+    }
+    return true;
+}
+
+function remove_tasmota(hostname)
+{
+    var tr_classname = get_tasmota_var(hostname,"tr_classname");
+    remove_all_with_classname(tr_classname);
+}
+function loadScriptAndShowPending(pending_id,scriptURL,name)
+{
+    dhtmlLoadScriptAddToQueueNamed(scriptURL,name);
+    pending_message(pending_id,"Pending ...");
+}
+function pending_message(pending_id,text)
+{
+    if(split_ids(pending_id,text,"pending_message"))
+        return;
+    
+    
+    if(pending_id.indexOf("_td_js_output")== -1)
+        pending_id += "_td_js_output";
+        
+    var obj = document.getElementById(pending_id);
+    if(!obj)
+    {
+        console.log(pending_id+" not found to place text: "+text);
+        return;
+    }
+    
+    animate_result_container(obj);
+    pending_backup(obj);
+    obj.innerHTML = text;
+}
+function clear_pending(obj)
+{
+    if(!obj.Pending)
+        return;
+    obj.Pending =false;
+    obj.innerHTML = obj.backupHTML;
+}
+function pending_backup(obj)
+{
+    if(obj.Pending)
+        return;
+    obj.Pending = true;
+    var cur_text = obj.innerHTML;
+    obj.backupHTML = cur_text;
+    
+}
+function toggle_backup(obj)
+{
+    if(obj.backupHTML)
+    {
+        obj.innerHTML = obj.backupHTML;
+        obj.backupHTML ="";
+        return true;
+    }
+    return false;
+    
+}
+function split_id(id)
+{
+    if(id.indexOf(id_delimiter)== -1)
+        return false;
+    return id.split(id_delimiter);
+}
+function split_ids(ids,text,split_mode)
+{
+    if(Array.isArray(ids))
+        var split = ids;
+    else
+        var split = split_id(ids);
+        
+    if(!split)
+        return false;
+    
+    switch(split_mode)
+    {
+        case "return_to_inner":
+            for(var i in split)
+                return_to_inner(split[i],text);
+            break;
+        case "append_to_inner":
+            for(var i in split)
+                append_to_inner(split[i],text);
+            break;
+        case "pending_message":
+            for(var i in split)
+                pending_message(split[i],text);
+            break;
+        default:
+            console.log("ERROR IN SPLIT_IDS: FUNCTION mode "+split_mode+" NOT FOUND");
+            return false;
+            
+    }
+    return true;
+}
+function return_to_inner(id,text)
+{
+    if(split_ids(id,text,"return_to_inner"))
+        return;
+    var obj = document.getElementById(id);
+    if(!obj)
+    {
+        //console.log(id+" not found to return");
+        return;
+    }
+    animate_result_container(obj);
+    
+    clear_pending(obj);
+    var new_div = document.createElement("div");
+    new_div.innerHTML = text;
+    new_div.className = "constrain_this";
+    var inner_divs = new_div.getElementsByTagName("div");
+    if(inner_divs.length)
+        obj.innerHTML = new_div.innerHTML;
+    else
+    {
+        obj.innerHTML = "";
+        obj.appendChild(new_div);
+    }
+        
+}
+
+function append_to_inner(id,text)
+{
+    if(split_ids(id,text,"append_to_inner"))
+        return;
+    
+    var obj = document.getElementById(id);
+    if(!obj)
+    {
+        //console.log(id+" not found to append");
+        return;
+    }
+    clear_pending(obj);
+    animate_result_container(obj);
+    var inner_divs = obj.getElementsByTagName("div");
+    
+    var new_div = document.createElement("div");
+    new_div.innerHTML = text;
+    new_div.className = "constrain_this";
+    
+    //var new_div_delimiters = new_div.getElementsByTagName("br");
+    
+    //if(new_div_delimiters.length)
+    //    var delimiter = "";
+    //else
+        var delimiter = "<br /><br />";
+    
+    var div = false;
+    if(inner_divs.length)
+        div = inner_divs[0];
+        
+    if(div)
+        div.innerHTML = text+delimiter+div.innerHTML ;
+    else
+    {
+        new_div.innerHTML = text+delimiter+obj.innerHTML;
+        obj.innerHTML = "";
+        obj.appendChild(new_div);
+    }
+    
+}
+function set_all_checkboxes_to_me(checkbox)
+{
+    var inputs = document.getElementsByTagName("input");
+    
+    var className = checkbox.className;
+    if(!className)
+    {
+        console.log("ERROR: class is not defined for checkbox, I am not clicking shit");
+        return false;
+        
+    }
+    
+    for(var i in inputs)
+        if(inputs[i].type == "checkbox" && inputs[i] != checkbox && 
+        (!className || inputs[i].classList.contains(className)))
+        {
+            inputs[i].checked = !checkbox.checked;
+            inputs[i].click();
+        }
+        
+}
+function exec_tasmota_relays_ip(command_string)
+{
+    exec_tasmota_relays(command_string,true);
+}
+function exec_tasmota_relays_hostname(command_string)
+{
+    exec_tasmota_relays(command_string,false);
+    
+}
+function exec_tasmota_relays(command_string,use_ip)
+{
+    var placeholder_num = 12345679;
+    if(command_string.indexOf(placeholder_num) == -1)
+    {
+        console.log("ERROR: Could not exec command on relays, placeholder "+placeholder_num+
+        " not found in command :"+command_string);
+        return false;
+    }
+    for(var hostname in tasmota_relays)
+    {
+        var host_relays = tasmota_relays[hostname];
+        for(var relay_num in host_relays)
+        {
+            if(!host_relays[relay_num])
+                continue;
+                
+            var cmnd = command_string.replace(placeholder_num,relay_num);
+            
+            exec_tasmota_queue(hostname,hostname,cmnd,use_ip);
+        }
+            
+    }
+    return true;
+}
+function exec_tasmota_ip(data_id,return_id,command_string)
+{
+    return exec_tasmota_queue(data_id,return_id,command_string,true);
+}
+
+function exec_tasmota_hostname(data_id,return_id,command_string)
+{
+    return exec_tasmota_queue(data_id,return_id,command_string,false);
+}
+
+function exec_tasmota_selected_hostname(command_string)
+{
+    exec_tasmota_selected(command_string,false);
+}
+function exec_tasmota_selected_ip(command_string)
+{
+    exec_tasmota_selected(command_string,true);
+    
+}
+
+function exec_tasmota_selected(command_string,use_ip)
+{
+    for(var hostname in tasmota_hostnames)
+    {
+        if(!tasmota_hostnames[hostname])
+            continue;
+            
+        exec_tasmota_queue(hostname,hostname,command_string,use_ip);
+    }
+}
+
+function exec_tasmota_queue(data_id,return_id,command_string,use_ip)
+{
+    tasmota_exec_queue.push([data_id,return_id,command_string,use_ip]);
+    console.log("tasmota_exec_queue: Added cmnd "+command_string+" for  " + data_id);
+    exec_tasmota_next(false);
+}
+function exec_tasmota_next(self_call)
+{
+    if(!tasmota_exec_queue.length)
+    {
+        console.log("tasmota_exec_queue finished.");
+        tasmota_queue_timer = false;
+        return;
+    }
+    if(tasmota_queue_timer && !self_call)
+        return;
+    
+    tasmota_queue_timer = setTimeout(function() { exec_tasmota_next(true); },tasmota_queue_spacing);
+    
+    this_command = tasmota_exec_queue.shift();
+    exec_tasmota(this_command[0],this_command[1],this_command[2],this_command[3]);
+
+}
+function exec_tasmota(data_id,return_id,command_string,use_ip)
+{
+    //list_name
+    //exfpath
+    if(!tasmota_exec_vars)
+    {
+        js_log("ERROR: tasmota_exec_vars not set");
+        return false;
+    }
+    var gets = Object.assign({},tasmota_exec_vars);
+    var exfpath = gets['exfpath'];
+    delete gets['exfpath'];
+    gets['return_id'] = encodeURIComponent(return_id);
+    
+    //device_username
+    //device_password
+    //protocol
+    if(!tasmota_device_vars)
+    {
+        console.log("ERROR: tasmota_device_vars not set");
+        return false;
+    }
+    var error = "";
+    while(true)
+    {
+        var username = "";
+        var password = "";
+        var ip = get_tasmota_var(data_id,"ip_address");
+        if(!ip)
+            error = "no ip_address in tasmota var ";
+        var hostname = get_tasmota_var(data_id,"hostname");
+        if(!hostname)
+            error = "no hostname_string found in tasmota var ";
+            
+        
+        if('username' in tasmota_device_vars)
+            username = encodeURIComponent(tasmota_device_vars['username']);
+        else
+            error = "no username found in tasmota var ";
+        
+        if('password' in tasmota_device_vars)
+            password = encodeURIComponent(tasmota_device_vars['password']);
+        else
+            error = "no password found in tasmota var ";
+            
+        var protocol = tasmota_device_vars['protocol'];
+        if(!protocol)
+            error = "no protocol found in tasmota var ";
+        command_string_encoded = encodeURIComponent(command_string);
+        if(!command_string)
+            error = "no command_string found in tasmota var ";
+        
+        var address = (use_ip)?ip:hostname.toLowerCase();
+        
+        var tasmota_url = protocol+"://"+address+"/cm?user="+username+"&password="+password+"&cmnd="+command_string_encoded;
+            
+        var exec_tasmota = btoa(tasmota_url);
+        gets['exec_tasmota'] = exec_tasmota;
+        gets['hostname'] = encodeURIComponent(hostname);
+        var js_url = exfpath+"?";
+        
+        for(var i in gets)
+        {
+            if(!gets[i])
+                error += " no "+i+" found in gets";
+            js_url+=i+"="+gets[i]+"&";
+        }
+        //error=tasmota_url;
+        if(error)
+            break;
+        var name = data_id+" cmnd: "+command_string;
+        loadScriptAndShowPending(return_id,js_url,name);
+        return true;
+    }
+    console.log("Command to "+return_id+" cmnd: '"+command_string+"' FAILED: "+error);
+    return false;
+    //
+}
+// return id should be HOSTNAME.. ish
+function get_tasmota_var(return_id,variable)
+{
+    var id = return_id+"_"+variable+"_JSON";
+    var json_div = document.getElementById(id);
+    if(!json_div)
+    {
+        console.log("could not get tasmota variable, id "+id+" not found");
+        return false;
+    }
+    var contents = json_div.innerHTML;
+    parsed = JSON.parse(contents);
+    return parsed;
+}
+function click_tasmota_command_buttons(id_suffix)
+{
+    timeout=0;
+    for(var hostname in tasmota_hostnames)
+    {
+        if(!tasmota_hostnames[hostname])
+            continue;
+        button = document.getElementById(hostname+id_suffix);
+        if(!button)
+        {
+            console.log(hostname+id_suffix+ " Not found");
+        continue;
+        }
+        tasmota_all_click_buttons.push(button);
+        setTimeout(click_next_tasmota_button,timeout);
+        timeout += 200;
+    }
+}
+function click_next_tasmota_button()
+{
+    if(!tasmota_all_click_buttons.length)
+    {
+        console.log("ERROR: NO BUTTON TO CLICK");
+        return;
+    }
+    this_but = tasmota_all_click_buttons.shift();
+    this_but.click();
+}
+function animate_result_container(obj)
+{
+    //obj.className='animated_result_container';
+    obj.classList.add('animated_result_container');
+    obj.classList.add('start');
+    setTimeout(function(){
+    obj.classList.remove('start')},100);
+}
---
+++ frankenyaml/extensions/tasmota_list.php
@@ -0,0 +1,359 @@
+<?php
+define("CALLED_FROM_YAML_HELPER", defined("IN_YAML_HELPER"));
+$in_yaml_helper = defined("IN_YAML_HELPER");
+$start_dir = "./";
+if (($base_filename = basename(__file__)) == basename($_SERVER["SCRIPT_FILENAME"])) {
+    define("IN_YAML_HELPER", true);
+    $start_dir = "../";
+    require_once ("../frankenyaml_includes.php");
+    define("TASMOTA_FUNCTIONS_DIRECTORY", "tasmota_functions");
+} else
+    define("TASMOTA_FUNCTIONS_DIRECTORY", EXTENSIONS_DIRECTORY .
+        "/tasmota_functions");
+
+define("START_DIR", $start_dir);
+
+$home_url = "./?extension=" . pretty_filename(($base_filename));
+
+define("HOME_URL", $home_url);
+
+foreach (scandir(TASMOTA_FUNCTIONS_DIRECTORY) as $filename) {
+    $path = TASMOTA_FUNCTIONS_DIRECTORY . '/' . $filename;
+    if (is_file($path) && substr($filename, -4) == ".php") {
+        require $path;
+    }
+}
+switch (hijack_page()) {
+    case "hard_redirect":
+        hard_redirect();
+    case "return":
+        return;
+
+    default:
+        break;
+}
+
+
+js_pass_exec_vars();
+
+$attrib = MAIN_TABLE_ATTRIBUTES;
+$attrib = create_element_attributes($attrib);
+echo "<table $attrib>";
+$attrib = MAIN_TABLE_TITLE_ATTRIBUTES;
+$attrib = create_element_attributes($attrib);
+
+echo '<tr><th ' . $attrib . '>TASMOTAS</th></tr>';
+
+echo '<tr>';
+$attrib = HORIZONTAL_RESULT_CAPTION_TH;
+$attrib = create_element_attributes($attrib);
+
+foreach (DISPLAY_VALUES as $key) {
+    $pretty_key = ucwords(str_replace("_", " ", $key));
+    echo "<th $attrib>" . $pretty_key . "</th>";
+
+}
+echo '</tr>';
+$vertical_count = count(DISPLAY_VALUES_VERTICAL);
+$rowspan = " rowspan=\"$vertical_count\" ";
+$item_count = 1;
+
+$select_side_column = array();
+$tasmota_list = get_tasmota_list();
+foreach ($tasmota_list as $index => $info) {
+    $item_data = array();
+    foreach (array_merge(DISPLAY_VALUES, DISPLAY_VALUES_VERTICAL,
+        DISPLAY_VERTICAL_RELAYS) as $info_key => $key) {
+        $info_key = is_numeric($info_key) ? $key : $info_key;
+        if (isset($info[$info_key]))
+            $value = $info[$info_key];
+        else
+            $value = "";
+        $item_data[$key] = $value;
+    }
+    $hostname = $item_data['hostname'];
+    $ip_address = $item_data['ip_address'];
+    //$$key
+    $tr_classname = tr_classname($hostname);
+    $tr_classname_attribute = " class=\"$tr_classname\" ";
+    if (!$hostname) {
+        echo '<tr ' . $tr_classname_attribute . '><td colspan="' . TABLE_COLSPAN . '">';
+        echo 'Corrupt entry removed: ' . smart_nl2br($info) . '</td></tr>';
+        remove_hostname_from_db($index);
+        continue;
+    }
+
+    $commands_enabled = true;
+    if ($hostname != $index && !SCAN_MODE) {
+        $commands_enabled = false;
+        echo '<tr ' . $tr_classname_attribute . '><td colspan="' . TABLE_COLSPAN . '">';
+        echo 'Hostname mismatch (' . "H: $hostname, I:$index)" .
+            ' disabling commands. Rescan to fix. Disabling command buttons.</td></tr>';
+    }
+
+
+    $return_id = $hostname;
+
+    // first col
+    $select = '<input type="checkbox" name="hostnames[]" value="' . $hostname .
+        '" onclick="add_hostname_to_list(this.value, this.checked)" class="hostname" />';
+
+    $select .= json_div($hostname, "tr_classname", $tr_classname);
+
+    $select_extra = SCAN_MODE ? "" : remove_tasmota_button($hostname);
+    if (!$select_extra && LIST_DISPLAY_MODE == "short") {
+        $attrib = TINY_RESULT_DIV_ATTRIBUTES;
+        $attrib['id'] = "{$hostname}_td_js_output";
+        $attrib = create_element_attributes($attrib);
+
+        $select_extra .= "<div $attrib>&nbsp;</div>";
+    }
+    $select .= $select_extra;
+
+    $item_data['select'] = $select;
+
+    $last_refresh = time_since($item_data['last_refresh']);
+
+
+    // last col
+    $item_data['other'] = make_tasmota_cmnd_input($hostname);
+    $horizontal_values = "";
+    foreach (DISPLAY_VALUES as $key) {
+        $data = $item_data[$key];
+        $attrib = HORIZONTAL_RESULT_ATTRIBUTES;
+        $attrib['id'] = $hostname . "_td_$key";
+        $attrib = create_element_attributes($attrib);
+        $horizontal_values .= "<td $attrib>" . ($data ? format_display_value($data, $key) :
+            "&nbsp;") . "</td>";
+    }
+
+    echo "<tr $tr_classname_attribute>";
+    echo $horizontal_values;
+    echo '</tr>';
+    //--------------------------------- below is for full display ------------------------
+    if (LIST_DISPLAY_MODE == "short")
+        continue;
+
+    $attrib = create_element_attributes(array_merge(JS_OUTPUT_ATTRIBUTES, array('id' =>
+            $hostname . "_td_js_output")));
+
+    $main_result_td = "<td $attrib>&nbsp;</td>";
+
+
+    $vertical_trs = array();
+    foreach (DISPLAY_VALUES_VERTICAL as $key) {
+        $this_tr = "";
+        $attrib = DISPLAY_VALUES_VERTICAL_CAPTION_ATTRIBUTES;
+        $attrib = create_element_attributes($attrib);
+        // SKIPS FIRST TR!
+        if ($vertical_trs)
+            $this_tr = "<tr $tr_classname_attribute>";
+        $this_tr .= "<td $attrib>";
+        $this_tr .= format_vertical_display_name($key, $hostname);
+
+
+        $attrib = DISPLAY_VALUES_VERTICAL_ATTRIBUTES_VALUE;
+        $attrib['id'] = $hostname . "_td_$key";
+        $attrib = create_element_attributes($attrib);
+
+        $this_tr .= "</td><td $attrib>";
+        if ($item_data[$key])
+            $this_tr .= format_display_value($item_data[$key], $key, $hostname, $key);
+        else
+            $this_tr .= "[No Saved Value]";
+
+        $this_tr .= "</td></tr>";
+        $vertical_trs[] = $this_tr;
+    }
+
+
+    // Pretty much just the relay
+    // I think it might break otherwise
+    $attrib = DISPLAY_VERTICAL_RELAYS_ATTRIBUTES;
+    $attrib['id'] = $hostname . "_td_RelayName";
+    $attrib = create_element_attributes($attrib);
+    $relay_td = "<td $attrib>";
+    $relay_td .= format_display_value($item_data['RelayName'], "RelayName", $hostname,
+        "RelayName");
+    $relay_td .= "</td>";
+
+
+    $command_buttons = array();
+
+    if ($commands_enabled) {
+        $i = 1;
+        foreach (COMMAND_BUTTONS as $info_key => $caption) {
+            if (strpos($info_key, "cm:") === 0) {
+                list($wut, $cmnd) = explode(":", $info_key, 2);
+                $value = command_button($cmnd, $caption, $hostname, $hostname);
+            } else
+                $value = "[$info_key:$caption]";
+            $command_buttons[] = $value;
+            $i++;
+        }
+
+        $psb = popup_select_buttons();
+        $command_buttons = array_merge($command_buttons, $psb);
+    } else {
+        $command_buttons[] = "[Commands Disabled]";
+    }
+
+
+    echo "<tr $tr_classname_attribute>";
+
+    $attrib = COMMAND_BUTTON_ATTRIBUTES;
+    $attrib = create_element_attributes($attrib);
+    echo "<td $attrib>";
+
+    $attrib = COMMAND_BUTTON_DIV_ATTRIBUTES;
+    $attrib = create_element_attributes($attrib);
+    echo "<div $attrib>";
+    echo implode("", $command_buttons);
+    echo "</div></td>";
+
+    echo $main_result_td;
+    echo $relay_td;
+    echo implode("", $vertical_trs);
+    echo '</tr>';
+
+    $item_count++;
+}
+$hostname = $return_id = false;
+
+$psb = popup_select_buttons("selected");
+$all_buttons = $psb;
+
+foreach (COMMAND_BUTTONS as $info_key => $key) {
+    $info_key = is_numeric($info_key) ? $key : $info_key;
+    if (strpos($info_key, "cm:") === 0) {
+        list($wut, $cmnd) = explode(":", $info_key, 2);
+        $value = command_button_selected($cmnd, $key);
+        if (SCAN_MODE && substr($key, -2) == "**") {
+            $onload_commands[] = js_exec_selected($cmnd);
+        } elseif (!SCAN_MODE && substr($key, -1) == "*") {
+            $onload_commands[] = js_exec_selected($cmnd);
+        }
+    } else
+        $value = "[$caption:$info_key]";
+    $all_buttons[] = $value;
+}
+
+
+$side_buttons = array();
+foreach (DISPLAY_VALUES_VERTICAL as $cmnd) {
+    if (!tasmota_has_reference($cmnd, 'bool'))
+        continue;
+    $value = command_button_selected($cmnd, $cmnd);
+    $side_buttons[] = $value;
+}
+
+$onload_commands = array();
+$onload_commands[] = js_exec_selected(DEFAULT_CMND);
+
+$select = '<input type="checkbox" onchange="set_all_checkboxes_to_me(this)" ';
+$select .= 'id="select_all_hostnames_checkbox" class="hostname" value="1" name="select_all_hostnames" />*ALL*
+    <script>newfun = function() { var checkitall= document.getElementById(\'select_all_hostnames_checkbox\'); checkitall.click();
+    ';
+$select .= implode("\n", $onload_commands);
+$select .= '};
+    addFunctionToOnload(newfun);' . "\n";
+
+$select .= '</script>';
+
+echo '<tr>';
+
+$attrib = BOTTOM_SELECT;
+$attrib = create_element_attributes($attrib);
+echo "<td $attrib>" . $select . "</td>";
+
+
+$attrib = BOTTOM_SMALL_HR_TD;
+$attrib = create_element_attributes($attrib);
+echo "<td $attrib><hr /></td>";
+
+
+$attrib = BOTTOM_MANUAL_COMMAND_TD_ATTRIBUTES;
+$attrib = create_element_attributes($attrib);
+echo "<td $attrib>";
+echo make_tasmota_cmnd_input(JAVASCRIPT_DUMP_ID_PREFIX);
+echo "</td>";
+
+
+$attrib = BOTTOM_SMALL_HR_TD;
+$attrib = create_element_attributes($attrib);
+echo "<td $attrib><hr /></td>";
+
+$attrib = BOTTOM_TITLE_TD;
+$attrib = create_element_attributes($attrib);
+echo "<td $attrib>Apply To All</td>";
+
+$attrib = BOTTOM_HR_TD;
+$hr_td_attrib = create_element_attributes($attrib);
+$attrib = BOTTOM_HR;
+$attrib = create_element_attributes($attrib);
+echo "<td $hr_td_attrib><hr $attrib /></td>";
+
+
+echo '</tr><tr>';
+
+$attrib = TASMOTA_JSCRIPT_FEED_ATTIBUTES;
+$attrib = create_element_attributes($attrib);
+
+echo "<td $attrib >&nbsp;</td>";
+
+$attrib = BOTTOM_BUTTON_AREA_ATTRIBUTES;
+$attrib = create_element_attributes($attrib);
+echo "<td  $attrib>";
+
+$attrib = BOTTOM_BUTTON_AREA_DIV_ATTRIBUTES;
+$attrib = create_element_attributes($attrib);
+echo "<div $attrib>";
+$i = 1;
+foreach ($all_buttons as $button) {
+    echo $button . ($i && !($i % BOTTOM_BUTTON_BREAKPOINT) ? "<br />" : " ");
+    $i++;
+}
+
+echo '</div>';
+
+echo '</td>';
+
+
+$attrib = BOTTOM_VERTICAL_BUTTONS_ATTRIBUTES;
+$attrib = create_element_attributes($attrib);
+echo "<td  $attrib>";
+$attrib = BOTTOM_VERTICAL_BUTTONS_DIV_ATTRIBUTES;
+$attrib = create_element_attributes($attrib);
+echo "<div $attrib>";
+echo implode("<br />", $side_buttons);
+echo '</div></td>';
+
+
+$attrib = BOTTOM_RELAY_TD_ATTRIBUTES;
+$attrib = create_element_attributes($attrib);
+echo "<td  $attrib>";
+$attrib = BOTTOM_RELAY_DIV_CONTAINER_ATTRIBUTES;
+$attrib = create_element_attributes($attrib);
+echo "<div $attrib>";
+if (LIST_DISPLAY_MODE == "full")
+    echo format_display_value('selected', 'RelayName', 'selected');
+echo '</div>';
+echo '</td>';
+
+
+echo '</tr>';
+
+echo '</table><br /><hr /><br />';
+//$rads = array("192.168.3." => "192.168.3.", "192.168.1." => "192.168.1.");
+//iradio_array("ip_prefix", $rads, "192.168.3.");
+form_close();
+echo '<table><tr><td valign="top">';
+display_add_tasmota_box();
+
+echo '</td><td width="30">&nbsp;</td><td valign="top">';
+
+display_tasmota_login();
+
+echo "</td></tr></table>";
+
+?>
